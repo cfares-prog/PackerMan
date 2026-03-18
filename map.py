08a2caa7dbee1766061d7832ec09d82e8eaf1ee1
Faust <cfares405@gmail.com>
2026-03-18 20:33:12 +0200

    MAP WORKS RAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- map.py
+++ map.py
@@ -7,31 +7,64 @@ M_WIDTH = 28
 M_HEIGHT = 31
 
 #pixels
-TEMP_WIDTH = 800
-TILE_WIDTH = 124
+TEMP_WIDTH = 1080
+TILE_WIDTH = 64
 
 #temp for testing
 WIN = pygame.display.set_mode((TEMP_WIDTH,TEMP_WIDTH))
 pygame.display.set_caption("packer test 1")
 
 BLACK = (0, 0, 0)
-
 class TileSprites:
     TILE_SIZE = 8
     SCALE = 3.25
+
     sheet = SpriteSheet("sprites.png")
-    test = sheet.get_sprite_by_grid(0,3,TILE_SIZE,TILE_SIZE)
-    hor_wall = sheet.get_sprite_scaled(27,27,TILE_SIZE,TILE_SIZE,SCALE)
-    ver_wall = sheet.get_sprite_scaled(117,27,TILE_SIZE,TILE_SIZE, SCALE)
-    inv_hor_wall = pygame.transform.flip(hor_wall,True,True)
-    inv_ver_wall = pygame.transform.flip(ver_wall,True,True)
+    test = sheet.get_sprite_by_grid(108,18,TILE_SIZE,TILE_SIZE)
+
+    ver_wall = sheet.get_sprite_scaled(27,27,TILE_SIZE,TILE_SIZE,SCALE)
+    hor_wall = sheet.get_sprite_scaled(117,27,TILE_SIZE,TILE_SIZE, SCALE)
+    inv_hor_wall = pygame.transform.flip(hor_wall, False, True)
+    inv_ver_wall = pygame.transform.flip(ver_wall, True, False)
+
     tleft_corner = sheet.get_sprite_scaled(9,27,TILE_SIZE,TILE_SIZE,SCALE)
     tright_corner = sheet.get_sprite_scaled(0,27,TILE_SIZE,TILE_SIZE,SCALE)
     bright_corner = sheet.get_sprite_scaled(36,27,TILE_SIZE,TILE_SIZE,SCALE)
     bleft_corner = sheet.get_sprite_scaled(45,27,TILE_SIZE,TILE_SIZE,SCALE)
+
+    t_right = sheet.get_sprite_scaled(63, 36, TILE_SIZE, TILE_SIZE, SCALE);
+    t_up = sheet.get_sprite_scaled(0, 0, TILE_SIZE, TILE_SIZE, SCALE);
+    t_left = sheet.get_sprite_scaled(0, 0, TILE_SIZE, TILE_SIZE, SCALE);
+    t_down = sheet.get_sprite_scaled(0, 0, TILE_SIZE, TILE_SIZE, SCALE);
+    cross = sheet.get_sprite_scaled(0, 0, TILE_SIZE, TILE_SIZE, SCALE);
+
     coin = sheet.get_sprite_scaled(117,45,TILE_SIZE,TILE_SIZE,SCALE)
     pellet = sheet.get_sprite_scaled(135,45,TILE_SIZE,TILE_SIZE,SCALE)
     gate = sheet.get_sprite_scaled(90,54,TILE_SIZE,TILE_SIZE,SCALE)
+
+WALL_MAPPING = {
+        # Straights
+        5: TileSprites.ver_wall,      
+        10: TileSprites.hor_wall,     
+
+        # Corners
+        6: TileSprites.tleft_corner,  
+        12: TileSprites.tright_corner,
+        9: TileSprites.bright_corner, 
+        3: TileSprites.bleft_corner,  
+
+        # Dead ends 
+        1: TileSprites.inv_ver_wall,      
+        4: TileSprites.inv_ver_wall,      
+        2: TileSprites.inv_hor_wall,      
+        8: TileSprites.inv_hor_wall,      
+
+        #Junctions
+        11: TileSprites.hor_wall,
+        14: TileSprites.inv_hor_wall,
+        7: TileSprites.ver_wall,
+        13: TileSprites.inv_ver_wall
+}
 
 class Tile(IntEnum):
     WALL  = 0
@@ -61,14 +94,16 @@ class TileMap:
                 "right" : (x + 1, y)
                 }
 
-    #helper method for draw
-    def get_wall_neighbors(self, x, y):
-        return {
-                "top"   : self.is_wall(x, y - 1),
-                "bottom": self.is_wall(x, y + 1),
-                "left"  : self.is_wall(x - 1, y),
-                "right" : self.is_wall(x + 1, y),
-                }
+    #draw helper
+    def get_tile_bitmask(self, row, col):
+        mask = 0
+        
+        if self.is_wall(row - 1, col): mask += 1
+        if self.is_wall(row, col + 1): mask += 2
+        if self.is_wall(row + 1, col): mask += 4
+        if self.is_wall(row, col - 1): mask += 8
+        
+        return mask
 
     def eat_coin(self, x, y):
 
@@ -112,55 +147,58 @@ TILES = [
         [Tile.WALL, Tile.WALL, Tile.WALL, Tile.WALL, Tile.WALL, Tile.WALL, Tile.WALL, Tile.WALL, Tile.WALL, Tile.WALL, Tile.WALL, Tile.WALL, Tile.WALL, Tile.WALL, Tile.WALL, Tile.WALL, Tile.WALL, Tile.WALL, Tile.WALL, Tile.WALL, Tile.WALL, Tile.WALL, Tile.WALL, Tile.WALL, Tile.WALL, Tile.WALL, Tile.WALL, Tile.WALL],
         ]
 
-def draw(win,map):
+def draw(win,map): 
     win.fill(BLACK)
-    gap = TEMP_WIDTH// M_HEIGHT
+    gap = TEMP_WIDTH// M_HEIGHT - 1
+
+
     sprites = TileSprites
+    offset_x = TileSprites.TILE_SIZE * 10
+    offset_y = TileSprites.TILE_SIZE * 4
 
     for row_index, row in enumerate(map.tiles):
         for col_index, tile in enumerate(row):
 
-            px = col_index * gap
-            py = row_index * gap
+            px = col_index * gap + offset_x
+            py = row_index * gap + offset_y
 
             if tile == Tile.WALL:
-                walls = map.get_wall_neighbors(row_index, col_index)
-
-                if walls["bottom"] and walls["top"] and walls["left"] and walls["right"]:
-                    pass
+                sprite = TileSprites.test
+                mask = map.get_tile_bitmask(row_index, col_index)
                 
-                elif walls["bottom"] and walls["right"]:
-                    win.blit(sprites.tleft_corner,(px,py))
+                if row_index == 0 and (col_index != 0 and col_index != M_WIDTH - 1):
+                    sprite = sprites.inv_hor_wall
+                elif row_index == M_HEIGHT - 1 and (col_index != 0 and col_index != M_WIDTH - 1):
+                    sprite = sprites.hor_wall
 
-                elif walls["top"] and walls["right"]:
-                    win.blit(sprites.tright_corner,(px,py))
-
-                elif walls["bottom"] and walls["left"]:
-                    win.blit(sprites.bleft_corner,(px,py))
-
-                elif walls["top"] and walls["left"]:
-                    win.blit(sprites.bright_corner,(px, py))
-
-                #straight walls
-                elif walls["top"] and walls["bottom"]:
-                    if row_index > col_index:
-                        win.blit(sprites.ver_wall,(px,py))
+                elif mask == 10:
+                    if row_index >= (M_HEIGHT) // 2:
+                        sprite = sprites.hor_wall
+                    elif not map.is_wall(row_index + 1, col_index):
+                        sprite = sprites.inv_hor_wall
                     else:
-                        win.blit(sprites.inv_ver_wall,(px,py))
+                        sprite = sprites.hor_wall
 
-                elif walls["left"] and walls["right"]:
-                    if row_index > col_index:
-                        win.blit(sprites.hor_wall,(px,py))
+                elif  mask == 5:
+                    if col_index == M_WIDTH - 1 and (row_index != 0 and row_index != M_HEIGHT -1):
+                        sprite = sprites.inv_ver_wall
+                    elif col_index >= (M_WIDTH - 1) // 2:
+                        sprite = sprites.inv_ver_wall
                     else:
-                        win.blit(sprites.inv_hor_wall,(px,py))
-                
-            if tile == Tile.COIN:
+                        sprite = sprites.ver_wall
+
+                else:
+                    sprite = WALL_MAPPING.get(mask, sprites.test)
+
+                win.blit(sprite, (px, py))
+
+            elif tile == Tile.COIN:
                win.blit(sprites.coin,(px,py))
 
-            if tile == Tile.POWER:
+            elif tile == Tile.POWER:
                win.blit(sprites.pellet,(px,py))
 
-            if tile == Tile.GATE:
+            elif tile == Tile.GATE:
                win.blit(sprites.gate,(px,py))
 
 
@@ -174,7 +212,6 @@ def main(win):
     while run:
         draw(win,game_map)
         for event in pygame.event.get():
-            # Program Kill Switch
             if (event.type == pygame.QUIT):
                 run= False
             

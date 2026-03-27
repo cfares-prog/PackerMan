de9f75b47eadbca24974c01c3bb49aa7bdd7c937
ElementaleLord <patrickfarah200515@outlook.com>
2026-03-27 19:47:25 +0200

    fixed eating problem
    he eats them just need to implement proper code to display Or powerUp and all that

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -1,5 +1,6 @@
 extends Area2D
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
-func _on_area_entered(_area: Area2D) -> void:
-	sprite_2d.visible= false;
+func _on_body_entered(body: Node2D) -> void:
+	print("+1 Coin")
+	queue_free()
